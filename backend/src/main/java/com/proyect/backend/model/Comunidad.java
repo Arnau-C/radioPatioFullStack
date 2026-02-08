54cd7484a14d5093fc15c951c84e0f48e211f3b7
Oscar Nunez <oscarnunezpino@gmail.com>
2026-02-08 12:00:28 +0100

    Interfaz de comunidad + Post Login + Y logica Backend, FALTA CONFIGURAR SECURITY

--- backend/src/main/java/com/proyect/backend/model/Comunidad.java
+++ backend/src/main/java/com/proyect/backend/model/Comunidad.java
@@ -1,15 +1,7 @@
 package com.proyect.backend.model;
 
 import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -32,18 +24,14 @@ public class Comunidad {
     @Column(nullable = false)
     private String direccion;
 
-    // Relación con Usuario (presidente), one-to-one porque cada comunidad tiene un presidente único y cada usuario puede ser presidente de una sola comunidad.
     @OneToOne
     @JoinColumn(name = "presidente_username", referencedColumnName = "username")
     private Usuario presidente;
 
-    @Column(nullable = false, unique = true, length = 10)
-    private String codigoInvitacion;
 
     @Column(nullable = false)
     private LocalDateTime fechaCreacion;
 
-    //Metodo que se ejecuta antes de persistir la entidad para establecer la fecha de creación
     protected void onCreate() {
         this.fechaCreacion = LocalDateTime.now();
     }   
