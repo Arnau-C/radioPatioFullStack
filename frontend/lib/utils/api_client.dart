4a938d3bd678586c8b685df5caa97ea73d8de2dc
Oscar Nunez <oscarnunezpino@gmail.com>
2026-05-09 10:34:07 +0200

    Guardo configuracion del api_client antes del merge

--- frontend/lib/utils/api_client.dart
+++ frontend/lib/utils/api_client.dart
@@ -11,7 +11,7 @@ class ApiClient {
     //   host = '10.0.2.2'; // Esto solo sirve para el emulador, no para el móvil real
     // }
     // http://127.0.0.1:8080/api
-    return 'http://192.120.239.57:8080/api';
-    //return 'https://radiopatiofullstackbackend.onrender.com/api';
+    //return 'http://192.120.239.57:8080/api';
+    return 'https://radiopatiofullstackbackend.onrender.com/api';
   }
 }
